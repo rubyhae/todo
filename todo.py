4cff5a1f2cdfeac44ea95524bd6be963244281cd
Ibiella <rubyhae@gmail.com>
2024-11-27 18:49:08 +0900

    ✨ Add_task 기능 구현 및 .gitignore 파일 추
    .gitignore 파일 추가하여 tasks.json 파일 숨김 처리 완료

--- todo.py
+++ todo.py
@@ -1,5 +1,23 @@
-def add_task(task_name):
-    pass
+import json
+import os #시스템 내부에 접근할 수 있는 권한
+
+TASK_FILE = 'tasks.json'
+
+def load_task():
+    if os.path.exists('tasks.json'):#파일이 있는 경우
+        with open(TASK_FILE, 'r', encoding='utf-8') as file: #file > open(~~)
+            return json.load(file) #json.load() 함수라고도 하지만 해당 형태는 메소드라고도 한다
+    return []
+
+def save_task(tasks): #add_task를 통해 전달받은 해야할 일을 파일에 저장하는 기능
+    with open(TASK_FILE, 'w', encoding='utf-8') as file: #file = open(TASK_FILE, 'w', encoding='utf-8')
+        json.dump(tasks, file, indent=4, ensure_ascii=False)
+
+def add_task(task_name): #할일 추가하는 함수
+    tasks = load_task() #파일이 있다면 불러옴
+    task = {'name': task_name, 'completed':False} #입력한 데이터가 추가됨
+    tasks.append(task)
+    save_task(tasks)
     
 def view_task():
     pass
@@ -24,7 +42,7 @@ def main():
         choice = input("원하는 작업을 선택해주세요 (1~5): ")
 
         if choice == '1':
-            task_name =  input("추가할 작업을 입력해주세요.: ") #파이썬 공부하기
+            task_name = input("추가할 작업을 입력해주세요.: ") #파이썬 공부하기
             add_task(task_name)
         elif choice == '2':
             view_task()
